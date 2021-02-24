b908c5fbd4054fd3d1eca8e9b10f645328e07f1d
Jazzuh <31289364+Jazzuh@users.noreply.github.com>
2021-02-24 17:12:02 +0000

    Add shell build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+rm -rf dist
+
+mkdir -p dist/fxmigrant
+
+pushd server
+dotnet publish -c Release
+popd
+
+cp __resource.lua dist/fxmigrant/
+cp helper.lua dist/fxmigrant/
+
+mkdir -p dist/fxmigrant/server/bin/Release/netstandard2.0/publish/
+
+cp -a server/bin/Release/netstandard2.0/publish dist/fxmigrant/server/bin/Release/netstandard2.0/
